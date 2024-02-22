caf3626cdf5868c7f4c2f4d0c81be0444dce5df9
Saul Dominguez <saul.vau@gmail.com>
2024-02-22 21:02:39 +0100

    clase hasta las 2030 con mongo creacion borrado modificacion de agentes, y cn metodo listar y filtrado

--- nodeapp/models/Agente.js
+++ nodeapp/models/Agente.js
@@ -5,10 +5,19 @@ const mongoose = require('mongoose');
 //definir el schema de los agentes
 
 const agenteSchema = mongoose.Schema({
-    name: String,
+    name: {type:String, unique: true},
     age: Number
 });
 //podemos hacer un dato requerido y que tenga unos requisitos se abresn llaves y se mira la docu
+
+//podemos crear un metodo para listar estatico que queremos que lo tengan todos los agentes en el modelo
+agenteSchema.statics.listar =  function(filtro, skip, limit){
+    const query = Agente.find(filtro);
+    query.skip(skip);
+    query.limit(limit);
+    return query.exec();
+}
+
 
 //crear el modelo de agente
 const Agente = mongoose.model('Agente', agenteSchema);
